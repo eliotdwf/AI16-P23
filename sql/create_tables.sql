9410597ff810d345c5c2104879b2c30a244a1c90
eliot <eliotdewulf91@gmail.com>
2023-03-23 16:43:04 +0100

    maj du script sql de création de tables

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,122 +1,120 @@
+
 DROP TABLE IF EXISTS DemandeDevenirRecruteur;
 DROP TABLE IF EXISTS DemandeCreationOrga;
 DROP TABLE IF EXISTS Candidature;
-DROP TABLE IF EXISTS Recruteur;
 DROP TABLE IF EXISTS OffreEmploi;
-DROP TABLE IF EXISTS Organisation;
 DROP TABLE IF EXISTS PieceDossier;
+DROP TABLE IF EXISTS RoleUtilisateur;
+DROP TABLE IF EXISTS Utilisateur;
+DROP TABLE IF EXISTS Role;
+DROP TABLE IF EXISTS Organisation;
 DROP TABLE IF EXISTS TypeOrganisation;
 DROP TABLE IF EXISTS TypeMetier;
 
-
-CREATE OR REPLACE TABLE Candidat(
-   email VARCHAR(50),
-   mdp VARCHAR(50) NOT NULL,
-   nom VARCHAR(50) NOT NULL,
-   prenom VARCHAR(50) NOT NULL,
-   tel VARCHAR(50),
-   dateCreation DATE NOT NULL,
-   actif BOOLEAN,
-   PRIMARY KEY(email)
+CREATE TABLE TypeMetier(
+    type_metier INT,
+    nom VARCHAR(50) NOT NULL,
+    PRIMARY KEY(type_metier)
 );
 
-CREATE OR REPLACE TABLE Administrateur(
-   email VARCHAR(50),
-   mdp VARCHAR(50) NOT NULL,
-   nom VARCHAR(50) NOT NULL,
-   prenom VARCHAR(50) NOT NULL,
-   tel VARCHAR(50),
-   dateCreation DATE NOT NULL,
-   actif BOOLEAN,
-   PRIMARY KEY(email)
+CREATE TABLE TypeOrganisation(
+    type_organisation INT,
+    nom VARCHAR(50) NOT NULL,
+    PRIMARY KEY(type_organisation)
 );
 
-CREATE OR REPLACE TABLE TypeMetier(
-   type_metier INT AUTO_INCREMENT,
-   nom VARCHAR(50) NOT NULL,
-   PRIMARY KEY(type_metier)
+CREATE TABLE PieceDossier(
+     piece_dossier INT,
+     chemin_fichier VARCHAR(100) NOT NULL,
+     description_fichier VARCHAR(500),
+     PRIMARY KEY(piece_dossier)
 );
 
-CREATE OR REPLACE TABLE TypeOrganisation(
-   type_organisation INT AUTO_INCREMENT,
-   nom VARCHAR(50) NOT NULL,
-   PRIMARY KEY(type_organisation)
+CREATE TABLE Role(
+     id INT,
+     nom VARCHAR(100) NOT NULL,
+     PRIMARY KEY(id)
 );
 
-CREATE OR REPLACE TABLE PieceDossier(
-   piece_dossier INT AUTO_INCREMENT,
-   fichier BLOB NOT NULL,
-   PRIMARY KEY(piece_dossier)
+CREATE TABLE Organisation(
+     organisation VARCHAR(50),
+     nom VARCHAR(50) NOT NULL,
+     siege_social VARCHAR(100) NOT NULL,
+     description VARCHAR(5000) NOT NULL,
+     chemin_logo VARCHAR(100),
+     type_organisation INT NOT NULL,
+     PRIMARY KEY(organisation),
+     FOREIGN KEY(type_organisation) REFERENCES TypeOrganisation(type_organisation)
 );
 
-CREATE OR REPLACE TABLE Organisation(
-   siren VARCHAR(50),
-   nom VARCHAR(50) NOT NULL,
-   siege_social VARCHAR(100) NOT NULL,
-   description VARCHAR(5000) NOT NULL,
-   type_organisation INT NOT NULL,
-   PRIMARY KEY(siren),
-   FOREIGN KEY(type_organisation) REFERENCES TypeOrganisation(type_organisation)
+CREATE TABLE Utilisateur(
+    candidat VARCHAR(50),
+    mdp VARCHAR(50) NOT NULL,
+    nom VARCHAR(50) NOT NULL,
+    prenom VARCHAR(50) NOT NULL,
+    tel VARCHAR(50),
+    dateCreation DATE NOT NULL,
+    actif BOOLEAN,
+    role INT NOT NULL,
+    organisation VARCHAR(50) NOT NULL,
+    PRIMARY KEY(candidat),
+    FOREIGN KEY(organisation) REFERENCES Organisation(organisation)
 );
 
-CREATE OR REPLACE TABLE OffreEmploi(
-   id_offre INT AUTO_INCREMENT,
-   intitule VARCHAR(50) NOT NULL,
-   statut_poste VARCHAR(50) NOT NULL,
-   resp_hierarchique VARCHAR(50),
-   lieu_mission VARCHAR(100) NOT NULL,
-   rythme VARCHAR(50) NOT NULL,
-   salaire VARCHAR(50) NOT NULL,
-   description VARCHAR(1000) NOT NULL,
-   etat VARCHAR(50) NOT NULL,
-   date_validite DATE NOT NULL,
-   pieces_requises_candidature VARCHAR(50) NOT NULL,
-   nb_pieces_dossier_candidature INT NOT NULL,
-   siren VARCHAR(50) NOT NULL,
-   type_metier INT,
-   PRIMARY KEY(id_offre),
-   FOREIGN KEY(siren) REFERENCES Organisation(siren),
-   FOREIGN KEY(type_metier) REFERENCES TypeMetier(type_metier)
+CREATE TABLE OffreEmploi(
+    offre_emploi INT,
+    intitule VARCHAR(50) NOT NULL,
+    statut_poste VARCHAR(50) NOT NULL,
+    resp_hierarchique VARCHAR(50),
+    lieu_mission VARCHAR(100) NOT NULL,
+    rythme VARCHAR(50) NOT NULL,
+    salaire VARCHAR(50) NOT NULL,
+    description VARCHAR(1000) NOT NULL,
+    etat VARCHAR(50) NOT NULL,
+    date_validite DATE NOT NULL,
+    pieces_requises_candidature VARCHAR(50) NOT NULL,
+    nb_pieces_dossier_candidature INT NOT NULL,
+    organisation VARCHAR(50) NOT NULL,
+    type_metier INT,
+    PRIMARY KEY(offre_emploi),
+    FOREIGN KEY(organisation) REFERENCES Organisation(organisation),
+    FOREIGN KEY(type_metier) REFERENCES TypeMetier(type_metier)
 );
 
-CREATE OR REPLACE TABLE Recruteur(
-   email VARCHAR(100),
-   mdp VARCHAR(50) NOT NULL,
-   nom VARCHAR(50) NOT NULL,
-   prenom VARCHAR(50) NOT NULL,
-   tel VARCHAR(50),
-   dateCreation DATE NOT NULL,
-   actif BOOLEAN,
-   siren VARCHAR(50) NOT NULL,
-   PRIMARY KEY(email),
-   FOREIGN KEY(siren) REFERENCES Organisation(siren)
+CREATE TABLE Candidature(
+    candidat VARCHAR(50),
+    offre_emploi INT,
+    piece_dossier INT,
+    date_candidature DATE NOT NULL,
+    PRIMARY KEY(candidat, offre_emploi, piece_dossier),
+    FOREIGN KEY(candidat) REFERENCES Utilisateur(candidat),
+    FOREIGN KEY(offre_emploi) REFERENCES OffreEmploi(offre_emploi),
+    FOREIGN KEY(piece_dossier) REFERENCES PieceDossier(piece_dossier)
 );
 
-CREATE OR REPLACE TABLE Candidature(
-   email VARCHAR(50),
-   id_offre INT,
-   piece_dossier INT,
-   date_candidature DATE NOT NULL,
-   PRIMARY KEY(email, id_offre, piece_dossier),
-   FOREIGN KEY(email) REFERENCES Candidat(email),
-   FOREIGN KEY(id_offre) REFERENCES OffreEmploi(id_offre),
-   FOREIGN KEY(piece_dossier) REFERENCES PieceDossier(piece_dossier)
+CREATE TABLE DemandeCreationOrga(
+    candidat VARCHAR(50),
+    date_demande DATE NOT NULL,
+    organisation VARCHAR(50) NOT NULL,
+    PRIMARY KEY(candidat),
+    FOREIGN KEY(candidat) REFERENCES Utilisateur(candidat),
+    FOREIGN KEY(organisation) REFERENCES Organisation(organisation)
 );
 
-CREATE OR REPLACE TABLE DemandeCreationOrga(
-   email VARCHAR(50),
-   date_demande DATE NOT NULL,
-   siren VARCHAR(50) NOT NULL,
-   PRIMARY KEY(email),
-   FOREIGN KEY(email) REFERENCES Candidat(email),
-   FOREIGN KEY(siren) REFERENCES Organisation(siren)
+CREATE TABLE DemandeDevenirRecruteur(
+    candidat VARCHAR(50),
+    date_demande DATE NOT NULL,
+    organisation VARCHAR(50) NOT NULL,
+    PRIMARY KEY(candidat),
+    FOREIGN KEY(candidat) REFERENCES Utilisateur(candidat),
+    FOREIGN KEY(organisation) REFERENCES Organisation(organisation)
 );
 
-CREATE OR REPLACE TABLE DemandeDevenirRecruteur(
-   email VARCHAR(50),
-   date_demande DATE NOT NULL,
-   siren VARCHAR(50) NOT NULL,
-   PRIMARY KEY(email),
-   FOREIGN KEY(email) REFERENCES Candidat(email),
-   FOREIGN KEY(siren) REFERENCES Organisation(siren)
+CREATE TABLE RoleUtilisateur(
+    candidat VARCHAR(50),
+    id INT,
+    PRIMARY KEY(candidat, id),
+    FOREIGN KEY(candidat) REFERENCES Utilisateur(candidat),
+    FOREIGN KEY(id) REFERENCES Role(id)
 );
+
